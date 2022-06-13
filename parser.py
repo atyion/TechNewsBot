4faf5de39d4c53cab6a758a114c8f84106e685f1
atyion <101365532+atyion@users.noreply.github.com>
2022-06-13 19:07:37 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,8 +1,10 @@
 import xml.etree.ElementTree as ET
-tree = ET.parse(r"path")
+tree = ET.parse(r"PATH")
 root = tree.getroot()
-for item in root.findall('item'):
+print(root.tag)
+print(root.attrib)
+for item in root.findall('channel/item'):
     title = item.find('title').text
     description = item.find('description').text
     link = item.find('link').text
-    print(title, description, link)
+    print(title, description, link, "\n\n")

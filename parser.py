8fff055b4d314d92d1d3a38365d406e19cadc873
atyion <101365532+atyion@users.noreply.github.com>
2022-08-02 13:15:39 +0200

    little update

--- parser.py
+++ parser.py
@@ -1,6 +1,9 @@
+from urllib.request import urlopen
 import xml.etree.ElementTree as ET
-tree = ET.parse(r"PATH")
-root = tree.getroot()
+read_input = input("Give me the rss's link: ") #Get in input rss's link
+url = urlopen('in') #Using urlopen to read link
+tree = ET.parse(url) #Using xml.etree.ElementTree to parse che xml file
+root = tree.getroot() #Using getroot to get the root of the xml file
 print(root.tag)
 print(root.attrib)
 for item in root.findall('channel/item'):

66cc5cc41baf10cb2cb1e4bc9c0ca0d60da507af
atyion <andrea.cicinelli2003@gmail.com>
2022-11-06 15:20:37 +0100

    continue

--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 import xml.etree.ElementTree as ET
 read_input = input("Give me the rss's link: ") #Get in input rss's link
-url = urlopen(read_input) #Using urlopen to read link
+url = urlopen('https://feeds.a.dj.com/rss/RSSWSJD.xml') #Using urlopen to read link
 tree = ET.parse(url) #Using xml.etree.ElementTree to parse che xml file
 root = tree.getroot() #Using getroot to get the root of the xml file
 print(root.tag)
@@ -10,4 +10,7 @@ for item in root.findall('channel/item'):
     title = item.find('title').text
     description = item.find('description').text
     link = item.find('link').text
+    date = item.find('pubDate').text
+    date = date[0:len(date)-6]
+    print(date)
     print(title, description, link, "\n\n")

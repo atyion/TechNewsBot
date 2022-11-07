a58a1516a021388756eb5296fe6e0dbf8d9f7213
atyion <andrea.cicinelli2003@gmail.com>
2022-11-07 23:03:03 +0100

    final bot

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from parser import *
 import time
-import telegram.ext
-oldtime = ""
-if __name__ == "__main__":
+from config import api
+import telebot
+
+
+bot = telebot.TeleBot(api)
+times=0
+oldnews = ""
+
+
+#Start command
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    global oldnews
+    bot.send_message(message.chat.id, "Hi! Since now I'll send you the laster news about tech!")
     while True:
-        time.sleep(60)
-        if(parser()[3]!=oldtime):
-            print(parser())
-            oldtime = parser()[3]
+        time.sleep(5)
+        if(parser()[3]!=oldnews):
+            bot.send_message(message.chat.id, "<b>"+parser()[0]+"</b>"+"\n"+"<em>"+parser()[1]+"</em>"+"\n\n"+"<a href='"+parser()[2]+"'>Read more about it</a>", parse_mode= 'HTML')
+            oldnews = parser()[3]
+
+
+
+
+bot.infinity_polling()

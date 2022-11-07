cf8e4dd6b4cf6f97a7fbbd46b3913bac79d35cf3
atyion <andrea.cicinelli2003@gmail.com>
2022-11-07 02:00:26 +0100

    update

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from parser import *
-
+import time
+oldtime = ""
 if __name__ == "__main__":
-    print(parser()[3])
+    while True:
+        time.sleep(60)
+        if(parser()[3]!=oldtime):
+            print(parser())
+            oldtime = parser()[3]

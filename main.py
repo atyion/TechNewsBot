434a022b5dcde1c818c2baec0eb0c33fba1c0639
atyion <andrea.cicinelli2003@gmail.com>
2022-11-10 16:14:40 +0100

    update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from parser import *
 import time
 from config import api
 import telebot
+from multiprocessing import Process
 
 print("Starting bot...\n")
 
@@ -16,12 +17,12 @@ oldnews = ""
 print(users)
 
 
-#Start command
+#Start command   
 @bot.message_handler(commands=['start'])
 def start_command(message):
     global oldnews
     bot.send_message(message.chat.id, "Hi! Since now I'll send you the lastes news about tech! \nIf you don't want more news write me /stop!")
-    if message.chat.id not in users:
+    if str(message.chat.id)+"\n" not in users:
         with open('chatid.txt', 'a') as file:
             users.append(str(message.chat.id)+'\n')
             file.write(str(message.chat.id)+"\n")
@@ -29,18 +30,23 @@ def start_command(message):
 
 
 #Update command
-@bot.message_handler(commands=(['update']))
+@bot.message_handler(commands=['update'])
 def update_command(message):
+    time.sleep(5)
+    print("entering...\n")
     global oldnews
     with open("news.txt",'r+') as file:
         oldnews = file.readline()
     if(parser()[0]!=oldnews):
         print("Sending news...\n")
-        for i in range(len(users)):
+        lenn = len(users)
+        for i in range(lenn):
             bot.send_message(users[i], "<b>"+parser()[0]+"</b>"+"\n"+"<em>"+parser()[1]+"</em>"+"\n\n"+"<a href='"+parser()[2]+"'>Read more about it</a>", parse_mode= 'HTML')
+            print("sent ", i, "/",lenn)
             with open("news.txt",'r+') as file:
                 file.truncate(0)
                 file.write(parser()[0])
+
 
 #Stop Command
 @bot.message_handler(commands=['stop'])
@@ -55,4 +61,5 @@ def stop_command(message):
                     file.write(users[i]+"\n")
     else:
         bot.send_message(message.chat.id, "Already stopped!")
+
 bot.infinity_polling()
